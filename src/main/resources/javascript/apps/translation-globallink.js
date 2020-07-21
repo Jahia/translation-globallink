98e5021da865f9681f26763a3f37d81d042532c5
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-07-21 17:47:56 +0200

    BACKLOG-14183 : add translation action from jcontent

--- src/main/resources/javascript/apps/translation-globallink.js
+++ src/main/resources/javascript/apps/translation-globallink.js
@@ -1,3 +1,22 @@
+window.jahia.uiExtender.registry.add('callback', 'translation-Globallink', {
+    targets: ['jahiaApp-init:3'],
+    callback: () => Promise.all([
+        window.jahia.uiExtender.registry.addOrReplace('action', 'createTranslation', {
+            ...window.jahia.uiExtender.registry.get('action', 'createContent'),
+            showOnNodeTypes: [],
+            hideOnNodeTypes: []
+        }, {
+            buttonIcon: window.jahia.moonstone.toIconComponent('GlobalLink'),
+            nodeTypes: ['gblnt:globalLinkProject'],
+            openEditor: true,
+            buttonLabel: 'jahia-translation-globallink:label.requestATranslation',
+            targets: ['contentActions:3.10'],
+            showOnNodeTypes: ['jnt:page'],
+            hideOnNodeTypes: ['jnt:contentFolder', 'jnt:content']
+        })
+    ])
+});
+
 window.jahia.i18n.loadNamespaces('jahia-translation-globallink');
 
 window.jahia.uiExtender.registry.add('adminRoute', 'translation-globallink-settings', {
@@ -20,3 +39,5 @@ window.jahia.uiExtender.registry.add('adminRoute', 'translation-globallink-reque
     requireModuleInstalledOnSite: 'jahia-translation-globallink',
     iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.globallink-translation-requests.html'
 });
+console.debug('%c GLobalLink translation is activated', 'color: #3c8cba');
+
