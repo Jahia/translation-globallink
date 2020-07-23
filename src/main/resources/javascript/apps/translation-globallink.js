ff71baa2365a06f563ca494d117e40b7a2572e4c
Taybou <benterki.tayeb@gmail.com>
2020-07-23 10:10:24 +0200

    BACKLOG-13970 move GLK entries to administration & jcontent

--- src/main/resources/javascript/apps/translation-globallink.js
+++ src/main/resources/javascript/apps/translation-globallink.js
@@ -1,16 +1,7 @@
 window.jahia.i18n.loadNamespaces('jahia-translation-globallink');
 
-window.jahia.uiExtender.registry.add('adminRoute', 'translation-globallink', {
-    targets: ['jcontent:23'],
-    icon: window.jahia.moonstone.toIconComponent('GlobalLink'),
-    label: 'jahia-translation-globallink:label.title',
-    isSelectable: false,
-    requiredPermission: 'adminGlobalLinkTranslation',
-    requireModuleInstalledOnSite: 'jahia-translation-globallink',
-});
-
 window.jahia.uiExtender.registry.add('adminRoute', 'translation-globallink-settings', {
-    targets: ['jcontent-translation-globallink:1'],
+    targets: ['administration-sites:110'],
     icon: window.jahia.moonstone.toIconComponent('GlobalLink'),
     label: 'jahia-translation-globallink:label.settings',
     isSelectable: true,
@@ -21,7 +12,7 @@ window.jahia.uiExtender.registry.add('adminRoute', 'translation-globallink-setti
 
 
 window.jahia.uiExtender.registry.add('adminRoute', 'translation-globallink-requests', {
-    targets: ['jcontent-translation-globallink:2'],
+    targets: ['jcontent:23'],
     icon: window.jahia.moonstone.toIconComponent('GlobalLink'),
     label: 'jahia-translation-globallink:label.requests',
     isSelectable: true,
