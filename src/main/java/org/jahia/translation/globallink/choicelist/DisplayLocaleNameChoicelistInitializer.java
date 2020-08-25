c64315f3f2d09654396e203f770565b3d2e04437
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-08-25 17:39:41 +0200

    BACKLOG-14504 : update initializer to allow to open request in edit mode

--- src/main/java/org/jahia/translation/globallink/choicelist/DisplayLocaleNameChoicelistInitializer.java
+++ src/main/java/org/jahia/translation/globallink/choicelist/DisplayLocaleNameChoicelistInitializer.java
@@ -9,33 +9,48 @@ import org.jahia.services.content.nodetypes.initializers.ModuleChoiceListInitial
 import org.jahia.translation.globallink.common.GlobalLinkConstants;
 import org.jahia.utils.LanguageCodeConverters;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Value;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by rincevent on 2016-07-21.
  */
-@Component(service = ModuleChoiceListInitializer.class, immediate = true)
-public class DisplayLocaleNameChoicelistInitializer implements ModuleChoiceListInitializer {
-    @Override
-    public String getKey() {
+@Component(service = ModuleChoiceListInitializer.class, immediate = true) public class DisplayLocaleNameChoicelistInitializer
+        implements ModuleChoiceListInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DisplayLocaleNameChoicelistInitializer.class);
+    public static final String SOURCE_LANGUAGE = "sourceLanguage";
+    public static final String CONTEXT_NODE = "contextNode";
+
+    @Override public String getKey() {
         return "displayLocaleName";
     }
 
-    @Override
-    public void setKey(String s) {
+    @Override public void setKey(String s) {
     }
 
-    @Override
-    public List<ChoiceListValue> getChoiceListValues(ExtendedPropertyDefinition extendedPropertyDefinition, String s, List<ChoiceListValue> list, Locale userLocale, Map<String, Object> map) {
+    private JCRSiteNode getResolveSite(Map<String, Object> map) throws RepositoryException {
+        return Optional.ofNullable(map.get("contextParent")).map(node -> (JCRNodeWrapper) node)
+                .orElse((JCRNodeWrapper) map.get(CONTEXT_NODE)).getResolveSite();
+    }
+
+    @Override public List<ChoiceListValue> getChoiceListValues(ExtendedPropertyDefinition extendedPropertyDefinition, String s,
+            List<ChoiceListValue> list, Locale userLocale, Map<String, Object> map) {
         List<ChoiceListValue> results = new ArrayList<>();
         try {
-            JCRNodeWrapper nodeWrapper = (JCRNodeWrapper) map.get("contextParent");
-            JCRSiteNode resolveSite = nodeWrapper.getResolveSite();
+            JCRSiteNode resolveSite = getResolveSite(map);
             if (list != null && !list.isEmpty()) {
-                if (resolveSite.isNodeType(GlobalLinkConstants.GBL_MIXIN_TYPE) && resolveSite.getProperty(GlobalLinkConstants.GBL_PROPERTY_ENABLE).getBoolean()) {
+                if (resolveSite.isNodeType(GlobalLinkConstants.GBL_MIXIN_TYPE) && resolveSite
+                        .getProperty(GlobalLinkConstants.GBL_PROPERTY_ENABLE).getBoolean()) {
                     Value[] mappings = resolveSite.getProperty("j:languageMappings").getValues();
                     for (ChoiceListValue choiceListValue : list) {
                         //Get the language code by removing -source or -target
@@ -43,35 +58,47 @@ public class DisplayLocaleNameChoicelistInitializer implements ModuleChoiceListI
                         List<String> sourceMapping = new LinkedList<>();
                         for (Value mapping : mappings) {
                             String mappingString = mapping.getString();
-                            if(mappingString.endsWith(languageCode)) {
+                            if (mappingString.endsWith(languageCode)) {
                                 sourceMapping.add(mappingString);
                             }
                         }
                         addResults(userLocale, results, sourceMapping);
                     }
                 }
-            } else if (map.containsKey("sourceLanguage")) {
-                String sourceLanguage = ((String) ((List) map.get("sourceLanguage")).get(0)).split("###")[1];
-                JCRNodeWrapper sourceLanguageNode = resolveSite.getNode(sourceLanguage + "-gblSource");
-                Value[] mappings = resolveSite.getProperty("j:languageMappings").getValues();
-                for (Value targetLanguages : sourceLanguageNode.getProperty("targetLanguages").getValues()) {
-                    String languageCode = targetLanguages.getString();
-                    List<String> sourceMapping = new LinkedList<>();
-                    for (Value mapping : mappings) {
-                        String mappingString = mapping.getString();
-                        if(mappingString.endsWith(languageCode)) {
-                            sourceMapping.add(mappingString);
-                        }
-                    }
-                    addResults(userLocale, results, sourceMapping);
+            } else if (map.containsKey(SOURCE_LANGUAGE)) {
+                String sourceLanguage = ((String) ((List) map.get(SOURCE_LANGUAGE)).get(0)).split("###")[1];
+                addResults(userLocale, results, getTargetLanguagesMapping(sourceLanguage, resolveSite));
+            } else if (map.get(CONTEXT_NODE) != null) {
+                final JCRNodeWrapper contextNode = (JCRNodeWrapper) map.get(CONTEXT_NODE);
+                if (contextNode.hasProperty(SOURCE_LANGUAGE)) {
+                    addResults(userLocale, results,
+                            getTargetLanguagesMapping(contextNode.getPropertyAsString(SOURCE_LANGUAGE).split("###")[1], resolveSite));
+                } else {
+                    results.add(new ChoiceListValue("Please select a source language first", "empty"));
                 }
             } else {
                 results.add(new ChoiceListValue("Please select a source language first", "empty"));
             }
         } catch (RepositoryException e) {
-            e.printStackTrace();
+            LOGGER.error("Error while initializing language mapping", e);
         }
         return results;
+    }
+
+    private List<String> getTargetLanguagesMapping(String sourceLanguage, JCRSiteNode resolveSite) throws RepositoryException {
+        List<String> sourceMapping = new LinkedList<>();
+        JCRNodeWrapper sourceLanguageNode = resolveSite.getNode(sourceLanguage + "-gblSource");
+        Value[] mappings = resolveSite.getProperty("j:languageMappings").getValues();
+        for (Value targetLanguages : sourceLanguageNode.getProperty("targetLanguages").getValues()) {
+            String languageCode = targetLanguages.getString();
+            for (Value mapping : mappings) {
+                String mappingString = mapping.getString();
+                if (mappingString.endsWith(languageCode)) {
+                    sourceMapping.add(mappingString);
+                }
+            }
+        }
+        return sourceMapping;
     }
 
     private void addResults(Locale userLocale, List<ChoiceListValue> results, List<String> sourceMapping) {
@@ -79,7 +106,8 @@ public class DisplayLocaleNameChoicelistInitializer implements ModuleChoiceListI
             String[] locales = s1.split("###");
             // Render the locale object in the current user language
             ChoiceListValue value = new ChoiceListValue();
-            value.setDisplayName(LanguageCodeConverters.getLocaleFromCode(locales[0]).getDisplayName(userLocale)+"/"+Locale.forLanguageTag(locales[1]).getDisplayName(userLocale));
+            value.setDisplayName(LanguageCodeConverters.getLocaleFromCode(locales[0]).getDisplayName(userLocale) + "/" + Locale
+                    .forLanguageTag(locales[1]).getDisplayName(userLocale));
             value.setStringValue(s1);
             results.add(value);
         }
