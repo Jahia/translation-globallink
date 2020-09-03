2252103dc5aadc24bfa9612489ea59149c28e196
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-09-03 10:07:08 +0200

    BACKLOG-14553 : fix text intructions

--- src/main/java/org/jahia/translation/globallink/service/impl/GlobalLinkSubmissionServiceImpl.java
+++ src/main/java/org/jahia/translation/globallink/service/impl/GlobalLinkSubmissionServiceImpl.java
@@ -254,7 +254,7 @@ public class GlobalLinkSubmissionServiceImpl implements GlobalLinkSubmissionServ
             projectRootNode.setProperty("name", submissionName);
 
             String pmNotes = "Translation for - " + pageTitle + (requestDTO.isChildIncluded() ? " with " : WITHOUT) + " sub pages\n"
-                    + "form the web site - " + siteNode.getServerName() + "( " + siteNode.getTitle() + " )";
+                    + "from the web site - " + siteNode.getServerName() + "( " + siteNode.getTitle() + " )";
 
             projectRootNode.setProperty("instructions", pmNotes);
 
