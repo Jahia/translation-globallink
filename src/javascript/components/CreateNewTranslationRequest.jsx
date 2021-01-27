9756ded3ccf27411d1209e07b8fc6d303051af67
David Griffon <dgriffon@jahia.com>
2021-01-27 16:40:14 +0100

    BACKLOG-15428 : remove context usage in jahia actions (#62)

--- src/javascript/components/CreateNewTranslationRequest.jsx
+++ src/javascript/components/CreateNewTranslationRequest.jsx
@@ -3,11 +3,11 @@ import * as PropTypes from 'prop-types';
 import {useSelector} from 'react-redux';
 import {useNodeChecks} from '@jahia/data-helper';
 
-export const CreateNewTranslationRequest = ({context, render: Render, loading: Loading, ...otherProps}) => {
+export const CreateNewTranslationRequest = ({render: Render, loading: Loading, ...otherProps}) => {
     const {language, siteKey, uilang} = useSelector(state => ({language: state.language, siteKey: state.site, uilang: state.uilang}));
 
     const res = useNodeChecks(
-        {path: context.path, language: language},
+        {path: otherProps.path, language: language},
         {
             requireModuleInstalledOnSite: ['jahia-translation-globallink'],
             getPrimaryNodeType: true
@@ -16,12 +16,12 @@ export const CreateNewTranslationRequest = ({context, render: Render, loading: L
     );
 
     if (Loading && (res.loading)) {
-        return <Loading context={context}/>;
+        return <Loading {...otherProps}/>;
     }
 
     if (res.error) {
         console.error('Error while loading globallink translations.com', res.error);
-        return <Render context={{...context, isVisible: false}}/>;
+        return <Render {...otherProps} isVisible={false}/>;
     }
 
     if (res.checksResult && !window.globallinkFolderId[siteKey]) {
@@ -31,25 +31,13 @@ export const CreateNewTranslationRequest = ({context, render: Render, loading: L
     // Use onClick to open content editor using the URL.
     // URL : const onClick = () => history.push(`/content-editor/${language}/create/${nodeInfo.node.uuid}/gblnt:globalLinkProject`);
     // Use onClick to open the modal window
-    const onClick = () => window.CE_API.create(res.node.uuid, context.path, siteKey, language, uilang, ['gblnt:globalLinkProject'], [], false);
+    const onClick = () => window.CE_API.create(res.node.uuid, otherProps.path, siteKey, language, uilang, ['gblnt:globalLinkProject'], [], false);
 
-    // In case context is available
-    if (context) {
-        return (
-            <Render context={{
-            ...context,
-            isVisible: res.checksResult && Boolean(window.globallinkFolderId[siteKey]),
-            onClick: onClick
-        }}/>
-        );
-    }
-
-    // In case no context (new Action framework props propagation).
     return (
         <Render
-        isVisible={res.checksResult && Boolean(window.globallinkFolderId[siteKey])}
-        onClick={onClick}
-        {...otherProps}
+            isVisible={res.checksResult && Boolean(window.globallinkFolderId[siteKey])}
+            onClick={onClick}
+            {...otherProps}
         />
     );
 };
@@ -59,7 +47,6 @@ CreateNewTranslationRequest.defaultProps = {
 };
 
 CreateNewTranslationRequest.propTypes = {
-    context: PropTypes.object.isRequired,
     render: PropTypes.func.isRequired,
     loading: PropTypes.func
 };
