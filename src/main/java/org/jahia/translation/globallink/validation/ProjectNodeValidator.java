d1bb54ed3adaf66e926f0517ab62a4d7caf1147a
Jonathan <jsinovassinnaik@jahia.com>
2021-02-02 11:51:38 +0100

    add staging repository id and add license file

--- src/main/java/org/jahia/translation/globallink/validation/ProjectNodeValidator.java
+++ src/main/java/org/jahia/translation/globallink/validation/ProjectNodeValidator.java
@@ -1,3 +1,27 @@
+/*
+ * /*
+ *  * ==========================================================================================
+ *  * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ *  * ==========================================================================================
+ *  *
+ *  *                                  http://www.jahia.com
+ *  *
+ *  * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ *  * ==========================================================================================
+ *  *
+ *  *     Copyright (C) 2002-2021 Jahia Solutions Group. All rights reserved.
+ *  *
+ *  *     This file is part of a Jahia's Enterprise Distribution.
+ *  *
+ *  *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *  *     contained in the Jahia Solutions Group Terms & Conditions as well as
+ *  *     the Jahia Sustainable Enterprise License (JSEL).
+ *  *
+ *  *     For questions regarding licensing, support, production usage...
+ *  *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *  *
+ *  * ==========================================================================================
+ *  */
 package org.jahia.translation.globallink.validation;
 
 import org.jahia.services.content.JCRNodeWrapper;
