a4e8db7e77e7cdf098a83fe737c55d93c6ca7c6f
jahia-carp <carp@jahia.com>
2023-11-28 18:14:59 +0100

    TECH-1442: Use directly moonstone components.

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -1,9 +1,10 @@
 import {registry} from '@jahia/ui-extender';
+import {toIconComponent} from '@jahia/moonstone';
 import {CreateNewTranslationRequest} from './components/CreateNewTranslationRequest';
 
 export default function () {
     registry.addOrReplace('action', 'createTranslation', {
-        buttonIcon: window.jahia.moonstone.toIconComponent(`<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        buttonIcon: toIconComponent(`<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                     <g clip-path="url(#clip0)">
                                                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M5.14749 1.02954L4.12931 1.02954V3.11503L2.93165 1.81104L2.21169 2.59492L3.40896 3.8985L1.49399 3.8985L1.49399 5.02042L4.12931 5.02042H5.14749H5.15941V3.8985H5.14749L5.14749 1.02954ZM5.95419 7.04129V5.91939H5.96647H9.61957V7.04138L7.70466 7.04129L8.90187 8.34495L8.18199 9.12878L6.98466 7.82513V9.9103H5.96647L5.95419 7.04129Z" fill="black"/>
                                                                         <path d="M6.97213 2.9868V1.02953L5.95402 1.02953L5.95402 3.8985L5.95386 5.02042H6.97213H9.61924V3.8985L7.80463 3.8985C7.80463 3.8985 10.2857 1.14286 12 0C10.2857 0.571429 6.97213 2.9868 6.97213 2.9868Z" fill="black"/>
@@ -22,7 +23,7 @@ export default function () {
 
     registry.add('adminRoute', 'translation-globallink-settings', {
         targets: ['administration-sites:110'],
-        icon: window.jahia.moonstone.toIconComponent('GlobalLink'),
+        icon: toIconComponent('GlobalLink'),
         label: 'jahia-translation-globallink:label.settings',
         isSelectable: true,
         requiredPermission: 'adminGlobalLinkTranslation',
@@ -77,7 +78,7 @@ export default function () {
 
     registry.add('adminRoute', 'translation-globallink-requests', {
         targets: ['jcontent:23'],
-        icon: window.jahia.moonstone.toIconComponent('GlobalLink'),
+        icon: toIconComponent('GlobalLink'),
         label: 'jahia-translation-globallink:label.requests',
         isSelectable: true,
         requiredPermission: 'adminGlobalLinkTranslation',
